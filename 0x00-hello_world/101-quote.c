c39350d4b1551298917ceb577b7cb338deb36958
houssam <upaca1@gmail.com>
2023-03-17 01:53:58 +0100

    hello c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
+/*
+ * File: 101-quote.c
+ * Auth: moamineayadi
+ */
 #include <unistd.h>
 /**
-*main-Prints out the last part of a quote in the standard error.
-*
-*Retun: 1 if success.
-*/
+ * main - Entry point
+ *
+ *
+ * Return: Always 1 (Failure)
+ */
 int main(void)
 {
 	write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-9\n", 
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			59);
 	return (1);
 }
