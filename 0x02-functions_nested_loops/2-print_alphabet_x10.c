33d7c025598f4ee375256583f1b0f11d2c17fd41
houssam <upaca1@gmail.com>
2023-03-21 19:21:32 +0000

    hello c 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print 10 times alphabet
+*
+*Return: 0 on success execution.
+*/
+
+void print_alphabet_x10(void)
+ {
+ 	int line;
+
+ 	for (line = 0; line <= 9; line++)
+ 	{
+ 		char letter = 'a';
+
+ 		while (letter != 'z' + 1)
+ 		{	
+ 		{
+ 			_putchar(letter);
+ 			letter++;
+ 		}
+ 		_putchar('\n');
+ 	}
+ }
+
