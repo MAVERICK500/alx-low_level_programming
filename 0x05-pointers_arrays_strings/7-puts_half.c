4fb45e8465c82b914988e53c5e583ac7047669f0
houssam <upaca1@gmail.com>
2023-03-27 19:00:27 +0000

    hello c 5

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str) {
+int length = 0;
+
+	while (*str != '\0') {
+		length++;
+		str++;
+	}
+	int half_length = length / 2;
+
+	str -= half_length;
+	while (*str != '\0') {
+		_putchar(*str);
+		str++;
+	}
+	 _putchar("\n");
+}
