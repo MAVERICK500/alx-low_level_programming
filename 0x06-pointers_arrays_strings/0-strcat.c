8a174ddfc22995a92bc7a51beb48ffa39eb09f7e
houssam <upaca1@gmail.com>
2023-03-29 15:45:12 +0000

    hello c6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,15 @@
 #include "main.h"
 
-
-/**
- * _strcat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- * Return: dest
-**/
-
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
 
-	i = 0;
-	while (dest[i])
-		i++;
-	dest[i] = ' ';
-	j = 0;
-	while (src[j])
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+    int index = 0, dest_len = 0;
+
+    while (dest[index++])
+        dest_len++;
+
+    for (index = 0; src[index]; index++)
+        dest[dest_len++] = src[index];
+
+    return (dest);
 }
