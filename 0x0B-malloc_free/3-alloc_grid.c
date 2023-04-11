5310c06b998a0ea040afc06cd4ee967b357facbf
maverick <55205houssam@gmail.com>
2023-04-11 16:20:10 +0000

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int **alloc_grid(int width, int height)
+{
+    int **array;
+
+    if (width <= 0 || height <= 0)
+        return (NULL);
+
+    array = malloc(sizeof(int *) * height);
+    if (!array)
+        return (NULL);
+    for (int i = 0; i < height; i++)
+    {
+        array[i] = malloc(sizeof(int) * width);
+        if (!array[i])
+        {
+            for (int j = 0; j < i; j++)
+            {
+                free(array[j]);
+            }
+            free(array[i]);
+            return (NULL);
+        }
+    }
+
+    for (int i = 0; array[i]; i++)
+        for (int j = 0; array[i][j]; j++)
+            array[i][j] = 0;
+    return (array);
+}
