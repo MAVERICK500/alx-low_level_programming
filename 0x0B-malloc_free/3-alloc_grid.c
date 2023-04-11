66d101679dd6cafe55eace9c8d1307fa37c182ae
maverick <55205houssam@gmail.com>
2023-04-11 16:33:40 +0000

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,49 +3,31 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **array;
+	
+	int **array;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    array = malloc(sizeof(int *) * height);
-    if (!array)
-        return (NULL);
-    for (int i = 0; i < height; i++)
-    {
-        array[i] = malloc(sizeof(int) * width);
-        if (!array[i])
-        {
-            for (int j = 0; j < i; j++)
-            {
-                free(array[j]);
-            }
-            free(array[i]);
-            return (NULL);
-        }
-    }
+	array = malloc(sizeof(int *) * height);
+	if (!array)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+	if (!array[i])
+	{
+		for (j = 0; j < i; j++)
+		{
+			free(array[j]);
+		}
+		free(array[i]);
+		return (NULL);
+	}
+	}
 
-    for (int i = 0; array[i]; i++)
-        for (int j = 0; array[i][j]; j++)
-            array[i][j] = 0;
-    return (array);
-}
-
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
-
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
+	for (i = 0; array[i]; i++)
+	for (j = 0; array[i][j]; j++)
+		array[i][j] = 0;
+	return (array);
 }
