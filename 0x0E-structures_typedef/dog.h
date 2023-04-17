aea5ef8612e487459af74caa1da59936e40c08e5
maverick <55205houssam@gmail.com>
2023-04-17 19:08:27 +0000

    strct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,10 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
+ * This structure represents a dog, with a name, age, and owner. It is used
+ * to hold information about individual dogs in a program. The structure is
+ * defined using three members: a character pointer for the name, a float for
+ * the age, and another character pointer for the owner.
  */
 struct dog
 {
@@ -14,4 +18,19 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
+
+
 #endif /* DOG_H */
