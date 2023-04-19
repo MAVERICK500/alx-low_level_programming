aa1e4c81bc19be5382e53b8d0b180aa8531a58de
maverick <55205houssam@gmail.com>
2023-04-19 16:52:18 +0000

    funptr

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,6 @@
  * @f: pointer to a function that takes a pointer to a character array
  *     and returns no value
  *
- * This function prints the specified name using the function pointed to by f.
- * If either the name or
- * f pointers are NULL, the function returns without printing.
  */
 
 void print_name(char *name, void (*f)(char *))
